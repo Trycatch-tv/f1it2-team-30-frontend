0d55caf05a342d0de42e84fb08352e9cbf9f3c24
Paula <paula1007002@gmail.com>
2023-06-16 20:14:37 -0500

    feat: :sparkles: Se creo la vista de las tablas de usurios y categorias, se le puso color y formato en el centro

--- src/Pages/Creators.jsx
+++ src/Pages/Creators.jsx
@@ -5,7 +5,7 @@ function Creators() {
       <AuthenticatedLayout
         header={
           <h2 className="font-semibold text-2xl text-gray-800 dark:text-[#404750] leading-tight">
-            Lista de Categorias
+            Creadores
           </h2>
         }
       >
@@ -13,7 +13,7 @@ function Creators() {
           <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
               <div className="p-6 text-gray-900 dark:text-gray-100">
-                CREators
+                
               </div>
             </div>
           </div>
